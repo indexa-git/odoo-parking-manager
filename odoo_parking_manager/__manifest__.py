fffbaf2052eec0c28f67f5f871efbd77ea0c6e43
Giame Fajardo <Giame Fajardo>
2022-05-25 12:22:38 -0400

    [ADD] odoo_parking_manager: Adding the directories of the module and initial files.

---
+++ odoo_parking_manager/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Odoo Parking Manager",
+    'summary': "",
+    'description': """
+    
+    
+    """,
+    'author': "CECOCARD Team",
+    'website': "",
+    'category': "",
+    'version': "0.1",
+    'depends': ['base']
+}
