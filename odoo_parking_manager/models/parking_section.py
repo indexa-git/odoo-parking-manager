719884d0bd1391d8c0c96cafe4ebd57d7de49bdb
Ana Paredes <anagparedesg@gmail.com>
2022-05-25 14:58:47 -0400

    [FIX] parking_slot_historical: add blank line at the end of the file

--- odoo_parking_manager/models/parking_section.py
+++ odoo_parking_manager/models/parking_section.py
@@ -2,10 +2,9 @@ from odoo import fields, models
 
 
 class ParkingSection(models.Model):
-    _name = 'parking.section'
+    _name = "parking.section"
 
     name = fields.Char(30, True, index=True)
     parent_id = fields.Many2one()
     children_ids = fields.One2many()
     slot_ids = fields.One2many()
-
