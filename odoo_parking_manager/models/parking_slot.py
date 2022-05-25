80350101c8b775cffa7b67c5041bd74897aba16f
Christopher Rodriguez <rodriguezchristopher10@gmail.com>
2022-05-25 14:30:54 -0400

    [REF] odoo_parking_manager: change occupant_id to partner_id, simplify the _description and use selection atribute

--- odoo_parking_manager/models/parking_slot.py
+++ odoo_parking_manager/models/parking_slot.py
@@ -3,12 +3,12 @@ from odoo import models, fields
 
 class ParkingSlot(models.Model):
     _name = "parking.slot"
-    _description = "Model for represent the parking slots registers."
+    _description = "Parking Slot"
 
     name = fields.Char(index=True)
     state = fields.Selection(
-        section=[("1", "Available"), ("2", "Taken"), ("3", "No Available")]
+        selection=[("1", "Available"), ("2", "Taken"), ("3", "No Available")]
     )
-    occupant_id = fields.Many2one("res.partner", ondelete="set null")
+    partner_id = fields.Many2one("res.partner", ondelete="set null", string="Occupant")
     section_id = fields.Many2one("parking.section", ondelete="set null")
     company_id = fields.Many2one("res.company", ondelete="set null")
