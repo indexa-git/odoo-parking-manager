f9bcd49c5c139d530201f8ef1c52ebbf9755a4e0
Giame Fajardo <Giame Fajardo>
2022-05-25 13:35:25 -0400

    [IMP] odoo_parking_manager: Making the 'name' field required

--- odoo_parking_manager/models/parking_section.py
+++ odoo_parking_manager/models/parking_section.py
@@ -4,7 +4,7 @@ from odoo import fields, models
 class ParkingSection(models.Model):
     _name = 'parking.section'
 
-    name = fields.Char(30, True, index=True)
+    name = fields.Char(30, True, index=True, required=True)
     parent_id = fields.Many2one()
     children_ids = fields.One2many()
     slot_ids = fields.One2many()
