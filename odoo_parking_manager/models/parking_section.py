f42275a3c2c96966813389749d4ecb9b793ffdfa
José López <joselopezg8@gmail.com>
2022-05-25 15:24:11 -0400

    [REF] odoo_parking_manager: flake8

--- odoo_parking_manager/models/parking_section.py
+++ odoo_parking_manager/models/parking_section.py
@@ -3,7 +3,7 @@ from odoo import fields, models
 
 class ParkingSection(models.Model):
     _name = "parking.section"
-    
+
     name = fields.Char(size=50, index=True, required=True)
     description = fields.Text()
     parent_id = fields.Many2one("parking.section", ondelete="set null")
