f1fdb40344d3a7ffaa91ecf98cb226d7dfbcbccd
Jose Lopez <joselopezg8@gmail.com>
2022-05-26 11:27:58 -0400

    [IMP] odoo_parking_manager: root menus & maker views

--- odoo_parking_manager/__manifest__.py
+++ odoo_parking_manager/__manifest__.py
@@ -8,5 +8,9 @@
     "category": "",
     "version": "14.0.0.0.1",
     "depends": ["base"],
-    "data": ["security/ir.model.access.csv"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/odoo_parking_manager_menu.xml",
+        "views/parking_vehicle_maker_views.xml",
+    ],
 }
