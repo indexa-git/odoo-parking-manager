29ed827c6e32cdc60faac6ea9b2cdd51c3e46ed0
Ana Paredes <anagparedesg@gmail.com>
2022-06-01 13:52:36 -0400

    [IMP] odoo_parking_manager: add function to report the changes made in a parking slot and extend the write function

--- odoo_parking_manager/models/parking_slot.py
+++ odoo_parking_manager/models/parking_slot.py
@@ -12,3 +12,26 @@ class ParkingSlot(models.Model):
     partner_id = fields.Many2one("res.partner", ondelete="set null", string="Occupant")
     section_id = fields.Many2one("parking.section", ondelete="set null")
     company_id = fields.Many2one("res.company", ondelete="set null")
+
+    def log_slot_historical(self, historical_type=""):
+        if not historical_type:
+            historical_type_mapping = {"1": "exit", "2": "entry"}
+
+            historical_type = historical_type_mapping[self.state]
+
+        self.env["parking.slot.historical"].create(
+            {
+                "slot_id": self.id,
+                "partner_id": self.partner_id.id,
+                "section_id": self.section_id.id,
+                "historical_type": historical_type,
+            }
+        )
+
+    def write(self, vals):
+        res = super(ParkingSlot, self).write(vals)
+
+        if vals.get("state"):
+            self.log_slot_historical()
+
+        return res
