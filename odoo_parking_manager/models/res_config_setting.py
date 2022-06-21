0c7ba8f7faa9225986957db47e6f48d043334727
Giame Fajardo <Giame Fajardo>
2022-06-21 11:18:08 -0400

    [IMP] odoo_parking_manager: Trying to make the menu to save.

--- odoo_parking_manager/models/res_config_setting.py
+++ odoo_parking_manager/models/res_config_setting.py
@@ -4,11 +4,8 @@ from odoo import models, fields
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    mandatory = fields.Boolean(string="Mandatory")
-    type = fields.Selection(
-            selection=[
-                ("gate", "Distance near to gate."),
-                ("exit", "Distance near to exit"),
-                ("exclusiveness", "Less exclusiveness"),
-            ]
-        )
+    odoo_parking_manager_mandatory = fields.Boolean(related="company_id.odoo_parking_manager_mandatory", readonly=False)
+    # odoo_parking_manager_type = fields.Selection(
+    #         related="company_id.odoo_parking_manager_type",
+    #         readonly=False,
+    #     )
